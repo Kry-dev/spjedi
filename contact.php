29c0e525fff996cf5d22e1e5e1993cd7f8ba2c4c
kry-dev <yaroslavkryvda@gmail.com>
2018-05-05 14:48:08 +0300

    new changes of content by Jed

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['email'])&&$_POST['email']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+    $to = 'yaroslavkryvda@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+    $subject = 'Обратный звонок'; //Загаловок сообщения
+    $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Name: '.$_POST['name'].'</p>
+                        <p>Email: '.$_POST['email'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+    $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+    $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+    mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
